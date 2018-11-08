827437f9311e729f3d83194da0776f3020f893d7
Mike Dilley <mike.dilley@gmail.com>
2018-11-08 18:38:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,11 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    # metaprogramming to create all attributes in hash that is passed in
+    student = Student.new
+    student_hash.each {|k,v| student.send("#{k}=",v)}
+    @@all << student 
+         
   end
 
   def self.create_from_collection(students_array)
